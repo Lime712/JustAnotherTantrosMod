7b98574d28a9e7ea660bf49b3e883213b56057f4
Lime712 <122940036+Lime712@users.noreply.github.com>
2024-01-04 02:09:29 +1300

    I'm shit at modding

--- src/jatm/content/JATMLiquids.java
+++ src/jatm/content/JATMLiquids.java
@@ -8,19 +8,19 @@ public class JATMLiquids{
     public static Liquid acid, brine; //methane
 
     public static void load(){
-        acid = new Liquid("acid", Color.valueOf("000000")){{
+        acid = new Liquid("acid", Color.valueOf("dce86e")){{
             heatCapacity = 0.3f;
             effect = StatusEffects.corroded;
             canStayOn.add(brine);
             boilPoint = 0.6f;
-            gasColor = Color.valueOf("000000");
+            gasColor = Color.valueOf("b8d16e");
         }};
 
-        brine = new Liquid("brine", Color.valueOf("000000")){{
-            heatCapacity = 0.25f;
+        brine = new Liquid("brine", Color.valueOf("ffcade")){{
+            heatCapacity = 0.5f;
             effect = StatusEffects.wet;
             boilPoint = 0.4f;
-            gasColor = Color.valueOf("000000");
+            gasColor = Color.valueOf("cfa2a5");
         }};
     }
 }
