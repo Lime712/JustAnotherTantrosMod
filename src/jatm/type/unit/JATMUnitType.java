29f367bdcfb04f970003708ae7f9ee288213c041
Lime712 <122940036+Lime712@users.noreply.github.com>
2024-01-16 18:01:52 +1300

    UnitType

---
+++ src/jatm/type/unit/JATMUnitType.java
@@ -0,0 +1,21 @@
+package jatm.type.unit;
+
+import mindustry.content.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.type.ammo.*;
+import mindustry.world.meta.*;
+import mindustry.entities.abilities.*;
+
+/** Tantros unit type, only functions underwater*/
+public class JATMUnitType extends UnitType{
+
+    public JATMUnitType(String name){
+        super(name);
+        outlineColor = Pal.darkOutline;
+        envRequired = envEnabled = Env.underwater;
+        ammoType = new ItemAmmoType(Items.aluminium);
+        researchCostMultiplier = 10f;
+        immunities.addAll(StatusEffects.wet, StatusEffects.shocked, StatusEffects.burning, StatusEffects.sporeSlowed);
+    }
+}
